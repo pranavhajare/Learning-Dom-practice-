fe0618510cf7f0a4527ffc2d85f686956733f4f0
pranavhajare <hajare.pranav@gmail.com>
2024-02-14 11:49:19 +0530

    keyboard

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,25 @@
 console.log('Project 5');
+console.log('Project 5');
+
+const insert = document.getElementById('insert');
+// console.log(insert);
+
+window.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  insert.innerHTML = `
+  <div class ="color">
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+</div>
+  `;
+});
